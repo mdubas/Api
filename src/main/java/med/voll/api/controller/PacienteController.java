021166f3daa484be27c7a2000b663ee8eca844d5
mdubas <415451@alunouninter.com>
2023-10-17 16:39:04 -0300

    Spring Boot.

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,4 +1,11 @@
 package med.voll.api.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/pacientes")
 public class PacienteController {
+
 }
