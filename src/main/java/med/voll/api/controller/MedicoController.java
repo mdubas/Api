4b1beaa8d451396130256e61ee05fe7147f26990
mdubas <415451@alunouninter.com>
2023-10-19 17:36:03 -0300

    Cadastros

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,5 +1,6 @@
 package med.voll.api.controller;
 
+import med.voll.api.medico.DadosCadastroMedico;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,10 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/medicos")
 public class MedicoController {
 
+    /*
     @PostMapping
     public void cadastrar(@RequestBody String json) {
         System.out.println(json); //request body para vincular a String ao corpo da requisição
     }
 }
-//no método acima é impresso o corpo da String, caso queira separar por atributo é necessário criar uma classe para isso.
-//start
+        No método acima é impresso o corpo da String, caso queira separar por atributo é necessário criar uma classe para isso.
+*/
+    @PostMapping
+    public void cadastrar(@RequestBody DadosCadastroMedico dados) {
+        System.out.println(dados);
+    }
+}
---
+++ src/main/java/med/voll/api/controller/paciente/DadosCadastroPaciente.java
@@ -0,0 +1,5 @@
+package med.voll.api.controller.paciente;
+
+public record DadosCadastroPaciente(String nome, String email, String telefone, String cpf) {
+
+}
