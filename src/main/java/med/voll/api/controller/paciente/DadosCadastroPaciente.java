ebab0a28f6e9f3d98d0a60c1e58f17f5a9fd2d62
mdubas <415451@alunouninter.com>
2023-10-25 10:10:22 -0300

    Cadastros

--- src/main/java/med/voll/api/controller/paciente/DadosCadastroPaciente.java
+++ src/main/java/med/voll/api/controller/paciente/DadosCadastroPaciente.java
@@ -1,5 +1,7 @@
 package med.voll.api.controller.paciente;
 
-public record DadosCadastroPaciente(String nome, String email, String telefone, String cpf) {
+import med.voll.api.endereco.DadosEndereco;
+
+public record DadosCadastroPaciente(String nome, String email, String telefone, String cpf, DadosEndereco endereco) {
 
 }
