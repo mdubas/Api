938df63f16614dc5953bd36106c04e900d20e078
Mdubas <4153451@alunouninter.com>
2023-10-25 19:29:48 -0300

    Estrutura

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,6 +1,6 @@
 package med.voll.api.controller;
 
-import med.voll.api.controller.paciente.DadosCadastroPaciente;
+import med.voll.api.paciente.DadosCadastroPaciente;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
